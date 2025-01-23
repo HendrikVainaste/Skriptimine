fe062fd2a1d51efccfbd55a33b87801986d584c1
Hendrik Väinaste <hendrik.vainaste@voco.ee>
2025-01-23 10:06:12 +0200

    Parandus failile

--- praks1/php_paigaldus.sh
+++ praks1/php_paigaldus.sh
@@ -4,14 +4,14 @@
 # kontrollime, mitu korda php7.0 korral ok installed
 # sõnad ol leitud ja vastus salvestame
 # muutuja sisse:
-PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
+PHP=$(dpkg-query -W -f='${Status}' php8.2 2>/dev/null | grep -c 'ok installed')
 # kui PHP muutuja väärtus võrdub 0-ga
 	if [ $PHP -eq 0 ]; then
 		# siis ok installed ei ole leitud
 		# ja väljastame vastava teate ning
 		# paigaldame teenuse
 		echo "Paigaldame php ja vajalikud lisad"
-		apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
+		apt install php8.2 libapache2-mod-php8.2 php8.2-mysql
 		echo "php on paigaldatud"
 #kui php muutuja väärtus võrdub 1-ga
 	elif [ $PHP -eq 1 ]; then
