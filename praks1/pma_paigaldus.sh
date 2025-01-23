6339766dda3c50faeeed2297846bc71dd455ca01
Hendrik Väinaste <hendrik.vainaste@voco.ee>
2025-01-23 12:19:04 +0200

    PHPAdmin lisamine

---
+++ praks1/pma_paigaldus.sh
@@ -0,0 +1,33 @@
+# phpmyadmin paigaldusskript
+#
+# kontrollime, mitu korda phpmyadmin korral ok installed
+# sõnad on leitud ja vastuse salvestame
+# muutuja sisse
+
+	PMA=$(dpkg-query -W -f='${Status}' phpmyadmin 2>/dev/null | grep -c 'ok installed')
+
+# kui PMA muutuja väärtus võrdub 0-ga
+
+	if [ $PMA -eq 0 ]; then
+
+	# siis ok installed ei ole leitud
+	# ja väljastame vastava teate 
+	# ning paigaldame teenuse
+
+		echo "Paigaldame phpmyadmin ja vajalikud lisad"
+		apt install phpmyadmin
+		echo "phpmyadmin on paigaldatud"
+
+# kui PMA muutuja väärtus võrdub 1-ga
+
+	elif [ $PMA -eq 1 ]; then
+
+	# siis ok installed leitud 1 kord
+	# ja teenus on juba paigaldatud
+
+		echo "phpmyadmin on juba paigaldatud"
+
+	#kontrollime olemasolu
+#lõpetame tingimuslause
+fi
+#skripti lõpp
